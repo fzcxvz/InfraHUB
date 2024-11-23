2ff17faaa4d4f173f0e118e2f326232d280d636d
fzcxvz <riccardo.pepe024@gmail.com>
2024-11-23 22:00:49 +0100

    webapp01

--- infrahub/app.py
+++ infrahub/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World!"
+
+
+@app.route("/template1")
+def layer1_template():
+    return render_template('layer1_template.html')
+
+@app.route("/template2")
+def layer2_template():
+    return render_template('layer2_template.html')
+
+@app.route("/template3")
+def layer3_template():
+    return render_template('layer3_template.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
